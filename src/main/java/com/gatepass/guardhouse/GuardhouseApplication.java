6106506863bd470870efa2b2b16b37413f7200c8
hanif <hanif.omar@neurogine.com>
2026-06-04 17:16:24 +0800

    initial commit

---
+++ src/main/java/com/gatepass/guardhouse/GuardhouseApplication.java
@@ -0,0 +1,13 @@
+package com.gatepass.guardhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuardhouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuardhouseApplication.class, args);
+	}
+
+}
