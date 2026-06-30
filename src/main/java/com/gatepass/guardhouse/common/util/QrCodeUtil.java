71f25f7d100b25c6b35a7bc421387b1a3f30382f
hanif <hanif.omar@neurogine.com>
2026-06-30 15:46:03 +0800

    feat(qr): add QrCodeUtil for generating QR data and PNG images

--- src/main/java/com/gatepass/guardhouse/common/util/QrCodeUtil.java
+++ src/main/java/com/gatepass/guardhouse/common/util/QrCodeUtil.java
@@ -1,7 +1,14 @@
 package com.gatepass.guardhouse.common.util;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 import org.springframework.stereotype.Component;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.time.Instant;
 
 @Component
@@ -11,8 +18,15 @@ public class QrCodeUtil {
         return String.join(":", "VISITOR", qrToken, expiry.toString());
     }
 
-    public byte[] generateQrCode(String data) {
-        return new byte[0];
+    public byte[] generateQrCode(String data) throws WriterException, IOException {
+
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(data, BarcodeFormat.QR_CODE, 300, 300);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", outputStream);
+
+        return outputStream.toByteArray();
     }
 
 
